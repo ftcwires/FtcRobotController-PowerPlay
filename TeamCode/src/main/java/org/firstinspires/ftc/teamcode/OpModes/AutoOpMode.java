dfb9c5ae0e9352bb3275c99fb97ea76a1d100f08
ftcwires <117595165+ftcwires@users.noreply.github.com>
2023-01-15 20:22:59 -0600

    Update AutoOpMode.java
    Faster cycling with reduced angles option

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AutoOpMode.java
@@ -89,33 +89,33 @@ public class AutoOpMode extends LinearOpMode{
                 initPose = new Pose2d(-54, 36, Math.toRadians(0)); //Starting pose
                 midWayPose = new Pose2d(-12, 36, Math.toRadians(0)); //Choose the pose to move forward towards signal cone
                 pickConePose = new Pose2d(-12, 55, Math.toRadians(90)); //Choose the pose to move to the stack of cones
-                dropConePose0 = new Pose2d(-12, 12, Math.toRadians(225)); //Choose the pose to move to the stack of cones
-                dropConePose1 = new Pose2d(-11, 12, Math.toRadians(225)); //Choose the pose to move to the stack of cones
-                dropConePose2 = new Pose2d(-10, 12, Math.toRadians(225)); //Choose the pose to move to the stack of cones
+                dropConePose0 = new Pose2d(-12, 12, Math.toRadians(135)); //Choose the pose to move to the stack of cones
+                dropConePose1 = new Pose2d(-11, 12, Math.toRadians(135)); //Choose the pose to move to the stack of cones
+                dropConePose2 = new Pose2d(-10, 12, Math.toRadians(135)); //Choose the pose to move to the stack of cones
                 break;
             case BLUE_RIGHT:
                 initPose = new Pose2d(-54, -36, Math.toRadians(0));//Starting pose
                 midWayPose = new Pose2d(-12, -36, Math.toRadians(0)); //Choose the pose to move forward towards signal cone
                 pickConePose = new Pose2d(-12, -55, Math.toRadians(270)); //Choose the pose to move to the stack of cones
-                dropConePose0 = new Pose2d(-12, -12, Math.toRadians(135)); //Choose the pose to move to the stack of cones
-                dropConePose1 = new Pose2d(-11, -12, Math.toRadians(135)); //Choose the pose to move to the stack of cones
-                dropConePose2 = new Pose2d(-10, -12, Math.toRadians(135)); //Choose the pose to move to the stack of cones
+                dropConePose0 = new Pose2d(-12, -12, Math.toRadians(225)); //Choose the pose to move to the stack of cones
+                dropConePose1 = new Pose2d(-11, -12, Math.toRadians(225)); //Choose the pose to move to the stack of cones
+                dropConePose2 = new Pose2d(-10, -12, Math.toRadians(225)); //Choose the pose to move to the stack of cones
                 break;
             case RED_LEFT:
                 initPose = new Pose2d(54, -36, Math.toRadians(180));//Starting pose
                 midWayPose = new Pose2d(12, -36, Math.toRadians(180)); //Choose the pose to move forward towards signal cone
                 pickConePose = new Pose2d(12, -55, Math.toRadians(270)); //Choose the pose to move to the stack of cones
-                dropConePose0 = new Pose2d(12, -12, Math.toRadians(45)); //Choose the pose to move to the stack of cones
-                dropConePose1 = new Pose2d(11, -12, Math.toRadians(45)); //Choose the pose to move to the stack of cones
-                dropConePose2 = new Pose2d(10, -15, Math.toRadians(45)); //Choose the pose to move to the stack of cones
+                dropConePose0 = new Pose2d(12, -12, Math.toRadians(315)); //Choose the pose to move to the stack of cones
+                dropConePose1 = new Pose2d(11, -12, Math.toRadians(315)); //Choose the pose to move to the stack of cones
+                dropConePose2 = new Pose2d(10, -15, Math.toRadians(315)); //Choose the pose to move to the stack of cones
                 break;
             case RED_RIGHT:
                 initPose = new Pose2d(54, 36, Math.toRadians(180)); //Starting pose
                 midWayPose = new Pose2d(12, 36, Math.toRadians(180)); //Choose the pose to move forward towards signal cone
                 pickConePose = new Pose2d(12, 55, Math.toRadians(90)); //Choose the pose to move to the stack of cones
-                dropConePose0 = new Pose2d(12, 12, Math.toRadians(315)); //Choose the pose to move to the stack of cones
-                dropConePose1 = new Pose2d(11, 12, Math.toRadians(315)); //Choose the pose to move to the stack of cones
-                dropConePose2 = new Pose2d(10, 12, Math.toRadians(315)); //Choose the pose to move to the stack of cones
+                dropConePose0 = new Pose2d(12, 12, Math.toRadians(45)); //Choose the pose to move to the stack of cones
+                dropConePose1 = new Pose2d(11, 12, Math.toRadians(45)); //Choose the pose to move to the stack of cones
+                dropConePose2 = new Pose2d(10, 12, Math.toRadians(45)); //Choose the pose to move to the stack of cones
                 break;
         }
 
@@ -133,27 +133,22 @@ public class AutoOpMode extends LinearOpMode{
                 })
                 //Uncomment following line to stop reduction in speed. And move to the position after which you want to stop reducing speed.
                 //.resetVelConstraint()
-                .lineToLinearHeading(midWayPose)
                 .lineToLinearHeading(pickConePose)
                 .addDisplacementMarker(() -> {
                     pickCone(1); //Pick top cone from stack
                 })
-                .lineToLinearHeading(midWayPose)
                 .lineToLinearHeading(dropConePose1)
                 .addDisplacementMarker(() -> {
                     dropCone(1); //Drop cone on junction
                 })
-                .lineToLinearHeading(midWayPose)
                 .lineToLinearHeading(pickConePose)
                 .addDisplacementMarker(() -> {
                     pickCone(2); //Pick second cone from stack
                 })
-                .lineToLinearHeading(midWayPose)
                 .lineToLinearHeading(dropConePose2)
                 .addDisplacementMarker(() -> {
                     dropCone(2); //Drop cone on junction
                 })
-                .lineToLinearHeading(midWayPose)
                 .build();
     }
 
